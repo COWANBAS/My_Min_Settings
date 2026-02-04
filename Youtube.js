fa53e10be47133ac8ab72c271617e3f7ad4769f5
Cowanbas <131304556+COWANBAS@users.noreply.github.com>
2026-02-04 16:47:05 -0300

    Add files via upload

---
+++ Youtube.js
@@ -0,0 +1,26 @@
+﻿// ==UserScript==
+// @name           Youtube Sempre Rodando
+// @description    Remove a mensagem "Continue assistindo" no YouTube e mantenha os vídeos reproduzindo
+// @namespace      CowanNEVERSTOP
+// @license        GPL-3.0
+// @version        1.0
+// @author         Cowanbas
+// @match          *://*/*
+// @run-at         document-start
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Função para remover a mensagem "Continue assistindo"
+  function removeContinueWatching() {
+    const continueButton = document.querySelector('button.ytp-continue-playback-button');
+    if (continueButton) {
+      continueButton.click();
+    }
+  }
+
+  // Define um intervalo para verificar a existência da mensagem a cada segundo
+  setInterval(removeContinueWatching, 1000);
+
+})();
